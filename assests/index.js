70ba7fa2471c9552ab5985e1a2013f7e95f25936
salahbm <salahbm.001@gmail.com>
2023-05-10 15:04:15 +0900

    AddRestaurant view changed

--- assests/index.js
+++ assests/index.js
@@ -1,8 +1,49 @@
 const map = require('../assests/map.png');
 const chinese = require('../assests/chinese.png');
 const asian = require('../assests/asian.jpg');
-const korean = require('../assests/korean.png');
-const western = require('../assests/western.png');
-// const chinese = require('../assests/chinese.jpg');
-// const chinese = require('../assests/chinese.jpg');
-export {map, chinese, asian, western, korean};
+const korean = require('../assests/korean.jpg');
+const western = require('../assests/western.jpg');
+const bunsik = require('../assests/bunsik.jpg');
+const burger = require('../assests/burger.jpg');
+const chicken = require('../assests/chicken.png');
+const cofee = require('../assests/cofee.jpg');
+const dessert = require('../assests/dessert.jpeg');
+const grilled = require('../assests/grilled.png');
+const jokbal = require('../assests/jokbal.jpg');
+const mexican = require('../assests/mexican.jpg');
+const lunchbox = require('../assests/lunchbox.jpg');
+const pizza = require('../assests/pizza.jpg');
+const porridge = require('../assests/porridge.jpg');
+const salad = require('../assests/salad.jpg');
+const sandwich = require('../assests/sandwich.jpg');
+const seafood = require('../assests/seafood.jpg');
+const snack = require('../assests/snack.jpg');
+const stew = require('../assests/stew.jpg');
+const sushi = require('../assests/sushi.jpg');
+const tonkatsu = require('../assests/tonkatsu.jpg');
+
+export {
+  map,
+  chinese,
+  asian,
+  western,
+  korean,
+  bunsik,
+  burger,
+  chicken,
+  cofee,
+  dessert,
+  grilled,
+  jokbal,
+  mexican,
+  lunchbox,
+  pizza,
+  porridge,
+  salad,
+  sandwich,
+  seafood,
+  snack,
+  stew,
+  sushi,
+  tonkatsu,
+};
